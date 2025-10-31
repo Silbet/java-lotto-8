e4695f039ab4d1375e0199d32485ea390eb6a528
Junbeom <warbringer98@gmail.com>
2025-10-31 17:35:40 +0900

    Feat: 당첨 번호 비교 기능 추가

---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class LottoGame {
+    private final Lotto winningLotto;
+
+    public LottoGame(Lotto lotto) {
+        this.winningLotto = lotto;
+    }
+
+    public List<Integer> matchNumber(Lotto lotto) {
+        Set<Integer> lottoNumber = new HashSet<>(lotto.getNumbers());
+        Set<Integer> winningNumber = new HashSet<>(winningLotto.getNumbers());
+        lottoNumber.retainAll(winningNumber);
+        return new ArrayList<>(lottoNumber);
+    }
+}
