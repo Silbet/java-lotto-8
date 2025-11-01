4154086645fb05cb4e07b55b4260d8b8650a4b3f
Junbeom <warbringer98@gmail.com>
2025-11-02 00:59:04 +0900

    Test: 2등 계산 테스트 추가

--- src/test/java/lotto/LottoRankCalculatorTest.java
+++ src/test/java/lotto/LottoRankCalculatorTest.java
@@ -9,15 +9,23 @@ public class LottoRankCalculatorTest {
     @Test
     void 당첨번호_6개가_일치하면_1등이다() {
         List<Integer> matchingNumber = List.of(1, 2, 3, 4, 5, 6);
-        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, true);
 
         assertThat(lottorank).isEqualTo(LottoRank.FIRST);
     }
 
     @Test
-    void 당첨번호_5개가_일치하면_3등이다() {
+    void 당첨번호_5개가_일치하고_보너스번호가_일치하면_2등이다() {
+        List<Integer> matchingNumber = List.of(22, 23, 24, 25, 26);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, true);
+
+        assertThat(lottorank).isEqualTo(LottoRank.SECOND);
+    }
+
+    @Test
+    void 당첨번호_5개가_일치하고_보너스번호가_일치하지않으면_3등이다() {
         List<Integer> matchingNumber = List.of(7, 8, 9, 10, 11);
-        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, false);
 
         assertThat(lottorank).isEqualTo(LottoRank.THIRD);
     }
@@ -25,7 +33,7 @@ public class LottoRankCalculatorTest {
     @Test
     void 당첨번호_4개가_일치하면_4등이다() {
         List<Integer> matchingNumber = List.of(12, 13, 14, 15);
-        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, false);
 
         assertThat(lottorank).isEqualTo(LottoRank.FOURTH);
     }
@@ -33,7 +41,7 @@ public class LottoRankCalculatorTest {
     @Test
     void 당첨번호_3개가_일치하면_5등이다() {
         List<Integer> matchingNumber = List.of(16, 17, 18);
-        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, false);
 
         assertThat(lottorank).isEqualTo(LottoRank.FIFTH);
     }
@@ -41,7 +49,7 @@ public class LottoRankCalculatorTest {
     @Test
     void 당첨번호_2개가_일치하면_꽝이다() {
         List<Integer> matchingNumber = List.of(19, 20);
-        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, false);
 
         assertThat(lottorank).isEqualTo(LottoRank.FAILURE);
     }
@@ -49,7 +57,7 @@ public class LottoRankCalculatorTest {
     @Test
     void 당첨번호_1개가_일치하면_꽝이다() {
         List<Integer> matchingNumber = List.of(21);
-        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, false);
 
         assertThat(lottorank).isEqualTo(LottoRank.FAILURE);
     }
@@ -57,7 +65,7 @@ public class LottoRankCalculatorTest {
     @Test
     void 당첨번호_0개가_일치하면_꽝이다() {
         List<Integer> matchingNumber = List.of();
-        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber, false);
 
         assertThat(lottorank).isEqualTo(LottoRank.FAILURE);
     }
