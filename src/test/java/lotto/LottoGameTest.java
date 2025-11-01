f80787a94a190f787ab15e72951f2ca3b8bc1015
Junbeom <warbringer98@gmail.com>
2025-11-01 16:00:51 +0900

    Style: 쓰지 않는 import문 삭제

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -2,9 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class LottoGameTest {
