33793d9a5682982eb6d53f11e9a260142954cb97
Junbeom <warbringer98@gmail.com>
2025-11-01 16:02:57 +0900

    Test: 등수 계산 테스트 추가 (2등제외)

---
+++ src/test/java/lotto/LottoRankCalculatorTest.java
@@ -0,0 +1,64 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class LottoRankCalculatorTest {
+    @Test
+    void 당첨번호_6개가_일치하면_1등이다() {
+        List<Integer> matchingNumber = List.of(1, 2, 3, 4, 5, 6);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+
+        assertThat(lottorank).isEqualTo(LottoRank.FIRST);
+    }
+
+    @Test
+    void 당첨번호_5개가_일치하면_3등이다() {
+        List<Integer> matchingNumber = List.of(7, 8, 9, 10, 11);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+
+        assertThat(lottorank).isEqualTo(LottoRank.THIRD);
+    }
+
+    @Test
+    void 당첨번호_4개가_일치하면_4등이다() {
+        List<Integer> matchingNumber = List.of(12, 13, 14, 15);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+
+        assertThat(lottorank).isEqualTo(LottoRank.FOURTH);
+    }
+
+    @Test
+    void 당첨번호_3개가_일치하면_5등이다() {
+        List<Integer> matchingNumber = List.of(16, 17, 18);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+
+        assertThat(lottorank).isEqualTo(LottoRank.FIFTH);
+    }
+
+    @Test
+    void 당첨번호_2개가_일치하면_꽝이다() {
+        List<Integer> matchingNumber = List.of(19, 20);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+
+        assertThat(lottorank).isEqualTo(LottoRank.FAILURE);
+    }
+
+    @Test
+    void 당첨번호_1개가_일치하면_꽝이다() {
+        List<Integer> matchingNumber = List.of(21);
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+
+        assertThat(lottorank).isEqualTo(LottoRank.FAILURE);
+    }
+
+    @Test
+    void 당첨번호_0개가_일치하면_꽝이다() {
+        List<Integer> matchingNumber = List.of();
+        LottoRank lottorank = LottoRankCalculator.rank(matchingNumber);
+
+        assertThat(lottorank).isEqualTo(LottoRank.FAILURE);
+    }
+}
