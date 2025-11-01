82b7eb79fb4caa82258781c3dcb710d7693c8d59
Junbeom <warbringer98@gmail.com>
2025-11-02 00:58:48 +0900

    Test: 보너스 번호 비교 테스트 추가

--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -6,26 +6,30 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class LottoGameTest {
+    Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    LottoGame lottoGame = new LottoGame(winningLotto, 7);
 
     @Test
-    void 당첨번호와_일치하는_숫자들이_반환된다() {
-        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    void 로또와_당첨번호의_교집합을_반환한다() {
         Lotto lotto = new Lotto(List.of(4, 5, 6, 7, 8, 9));
-
-        LottoGame lottoGame = new LottoGame(winningLotto);
 
         List<Integer> numbers = lottoGame.matchNumber(lotto);
         assertThat(numbers).contains(4, 5, 6);
     }
 
     @Test
-    void 당첨번호와_일치하지_않으면_빈리스트가_반환된다() {
-        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    void 로또와_당첨번호가_공집합이면_빈리스트를_반환한다() {
         Lotto lotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
-
-        LottoGame lottoGame = new LottoGame(winningLotto);
 
         List<Integer> numbers = lottoGame.matchNumber(lotto);
         assertThat(numbers).isEmpty();
+    }
+
+    @Test
+    void 로또에_보너스숫자가_있으면_참을_반환한다() {
+        Lotto lotto = new Lotto(List.of(4, 5, 6, 7, 8, 9));
+
+        boolean bonus = lottoGame.matchBonusNumber(lotto);
+        assertThat(bonus).isTrue();
     }
 }
