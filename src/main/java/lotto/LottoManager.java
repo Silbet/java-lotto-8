642dc2f2adb573ca21a15c960ccf4439b7fb6591
Junbeom <warbringer98@gmail.com>
2025-11-03 13:59:51 +0900

    Comment: 로또 프로그램 로직 흐름 추가

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -9,11 +9,12 @@ public class LottoManager {
     public void run() {
         // 금액 입력 받기 : InputView -> int money
         // 로또 구매하기(금액+발행) : purchaseLottos()만들기. (countPuchasLottos, issueLottos)
+        // 구매 로또 출력하기 : OutputView
         // 번호들 입력받아 로또게임 생성하기. : InputView -> LottoGame()
         // 로또 결과 확인하기. : List<LottoRank> checkLottoResult()만들기. (반복문으로 matchNumber, matchBonusNumber, rank)
         // 당첨 내역 출력 : OutputView
-        // 수익률 계산
-        // 수익률 출력
+        // 수익률 계산 : calculateEarnings()만들기.
+        // 수익률 출력 : OutputView
     }
 
     private int countPurchaseLottos(int money) {
