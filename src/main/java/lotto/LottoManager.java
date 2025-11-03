cda6dbd01c8316d82bcda198964de5b1964d81d1
Junbeom <warbringer98@gmail.com>
2025-11-03 22:19:47 +0900

    Feat: 프로그램 전체 흐름 구현

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoManager {
     private static final int LOTTO_PRICE = 1000;
@@ -11,14 +13,23 @@ public class LottoManager {
     }
 
     public void run() {
-        // 금액 입력 받기 : InputView -> int money
-        // 로또 구매하기(금액+발행) : purchaseLottos()만들기. (countPuchasLottos, issueLottos)
-        // 구매 로또 출력하기 : OutputView
-        // 번호들 입력받아 로또게임 생성하기. : InputView -> LottoGame()
-        // 로또 결과 확인하기. : List<LottoRank> checkLottoResult()만들기. (반복문으로 matchNumber, matchBonusNumber, rank)
+        // 로또 구매하기
+        List<Lotto> lottos = purchaseLotto();
+
+        // 당첨번호, 보너스번호 입력받아 로또게임 생성하기
+        LottoGame lottogame = createLottoGame();
+
+        // 로또 게임 진행하기
+        Map<LottoRank, Integer> lottoResult = palyLottoGame(lottogame, lottos);
+
         // 당첨 내역 출력 : OutputView
+        OutputView.printWinningResult(lottoResult);
+
         // 수익률 계산 : calculateEarnings()만들기.
+        double earnings = calculateEarnings(lottoResult);
+
         // 수익률 출력 : OutputView
+        OutputView.printEarningsRate(earnings);
     }
 
     private int countPurchaseLottos(int money) {
@@ -31,5 +42,55 @@ public class LottoManager {
             Lottos.add(LottoGenerator.generate());
         }
         return Lottos;
+    }
+
+    private List<Lotto> purchaseLotto() {
+        OutputView.printPurchaseMoneyRequest();
+
+        int money = InputView.inputPurchaseMoney();
+        int count = countPurchaseLottos(money);
+        OutputView.printCheckPurchaseLottoCount(count);
+
+        List<Lotto> Lottos = issueLottos(count);
+        OutputView.printLottoNumbers(Lottos);
+
+        return Lottos;
+    }
+
+    private LottoGame createLottoGame() {
+        OutputView.printWinningNumberRequest();
+        Lotto winningLotto = new Lotto(InputView.inputWinningNumbers());
+
+        OutputView.printBonusNumberRequest();
+        int bonusNumber = InputView.inputBonusNumbers(winningLotto.getNumbers());
+
+        return new LottoGame(winningLotto, bonusNumber);
+    }
+
+    private Map<LottoRank, Integer> palyLottoGame(LottoGame lottogame, List<Lotto> lottos) {
+        Map<LottoRank, Integer> matchingResult = new HashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            matchingResult.put(rank, 0);
+        }
+
+        for (Lotto lotto : lottos) {
+            List<Integer> matchingNumber = lottogame.matchNumber(lotto);
+            boolean matchingBonusNumber = lottogame.matchBonusNumber(lotto);
+            LottoRank rank = LottoRankCalculator.rank(matchingNumber, matchingBonusNumber);
+
+            matchingResult.put(rank, matchingResult.get(rank) + 1);
+        }
+        return matchingResult;
+    }
+
+    private double calculateEarnings(Map<LottoRank, Integer> lottoResult) {
+        int totalLottoCount = 0;
+        for (Integer count : lottoResult.values()) {
+            totalLottoCount += count;
+        }
+        int totalMoney = totalLottoCount * LOTTO_PRICE;
+
+        int earnings = LottoEarningsCalculator.calculatorTotal(lottoResult);
+        return LottoEarningsCalculator.calculatorRate(totalMoney, earnings);
     }
 }
