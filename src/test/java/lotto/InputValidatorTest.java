2e9ccfa2c84f34cd6e1192b1eb7dadb643671a8f
Junbeom <warbringer98@gmail.com>
2025-11-03 19:48:15 +0900

    Feat: 당첨 번호 입력 테스트 추가

--- src/test/java/lotto/InputValidatorTest.java
+++ src/test/java/lotto/InputValidatorTest.java
@@ -49,4 +49,35 @@ public class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(InputValidator.ERROR_MESSAGE_PURCHASE_MONEY_UNIT);
     }
+
+
+    @Test
+    void 당첨_번호가_정수가_아니면_에러가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,2,3,4,5,육"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputValidator.ERROR_MESSAGE_INTEGER);
+    }
+
+    @Test
+    void 당첨_번호가_기준값_사이에_없으면_에러가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("0,2,3,4,5,46"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputValidator.ERROR_MESSAGE_OUT_OF_RANGE);
+    }
+
+    @Test
+    void 당첨_번호가_6개가_아니면_에러가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputValidator.ERROR_MESSAGE_INVALID_LOTTO_COUNT);
+    }
+
+    @Test
+    void 당첨_번호가_중복되면_에러가_발생한다() {
+        assertThatThrownBy(() -> InputValidator.validateWinningNumbers("1,1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(InputValidator.ERROR_MESSAGE_DUPLICATE_NUMBER);
+    }
+
+
 }
