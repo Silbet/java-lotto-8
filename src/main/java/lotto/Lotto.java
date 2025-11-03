dff44d67456ac10bc1be8084856efb7baa0640c2
Junbeom <warbringer98@gmail.com>
2025-11-03 22:58:53 +0900

    Feat: 로또 자체 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,12 @@
 package lotto;
 
+import static lotto.InputValidator.ERROR_MESSAGE_DUPLICATE_NUMBER;
+import static lotto.InputValidator.ERROR_MESSAGE_INVALID_LOTTO_COUNT;
+import static lotto.InputValidator.ERROR_MESSAGE_OUT_OF_RANGE;
+import static lotto.InputValidator.LOTTO_MAX_NUMBER;
+import static lotto.InputValidator.LOTTO_MIN_NUMBER;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -16,7 +23,15 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_LOTTO_COUNT);
+        }
+
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_DUPLICATE_NUMBER);
+        }
+
+        if (numbers.stream().anyMatch(n -> n < LOTTO_MIN_NUMBER || n > LOTTO_MAX_NUMBER)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_OUT_OF_RANGE);
         }
     }
 }
